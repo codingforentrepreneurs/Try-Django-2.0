531c9d495da8fac736fcd66952e995b6236ac2be
Justin Mitchel <jmitchel3@gmail.com>
2018-04-16 16:12:01 -0700

    17 - Template Context

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,7 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def about_us_view(request):
-    return render(request, "about.html", {})
+    print(request.user)
+    print(request.user.is_authenticated) # is_authenticated()
+    the_username = request.user
+    is_logged_in = request.user.is_authenticated
+    context = {
+        "username": the_username,
+        "logged_in": is_logged_in
+    }
+    return render(request, "about.html", context)
 
 def contact_us_view(request):
     return render(request, "contact.html", {})
